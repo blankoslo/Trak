35b727cd7f03a728ec6c5534d5e676a48f7addf4
Zenjjim <zaim.imran@gmail.com>
2021-02-26 14:24:28 +0100

    refactor: move function out of files

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,7 @@
+import axios from 'axios';
+import { Dispatch, SetStateAction } from 'react';
+import { IEmployeeTask } from 'utils/types';
+
 export const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export const axiosBuilder = (axiosFunc: Promise<unknown>, text: string, router, showProgressbar, showSnackbar, closeModal) => {
@@ -13,5 +17,26 @@ export const axiosBuilder = (axiosFunc: Promise<unknown>, text: string, router, 
     .catch((error) => {
       showProgressbar(false);
       showSnackbar(error.response.data.message, 'error');
+    });
+};
+
+export const toggleCheckBox = (
+  employeeTask: IEmployeeTask,
+  completed: boolean,
+  setCompleted: Dispatch<SetStateAction<boolean>>,
+  showSnackbar: (arg0: string, arg1: string) => void,
+) => {
+  axios
+    .put(`/api/employeeTasks/${employeeTask.id}`, {
+      completed: !completed,
+      dueDate: employeeTask.dueDate,
+      responsibleId: employeeTask.responsibleId,
+    })
+    .then(() => {
+      showSnackbar(`Oppgave ${completed ? 'ikke' : ''} fullført`, 'success');
+      setCompleted(!completed);
+    })
+    .catch((error) => {
+      showSnackbar(error.response.data?.message, 'error');
     });
 };
