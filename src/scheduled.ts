dae4377598701de18f5c647568cdb67224bda5cd
Zenjjim <zaim.imran@gmail.com>
2021-03-10 11:27:33 +0100

    chore: cron

--- src/scheduled.ts
+++ src/scheduled.ts
@@ -1,5 +1,16 @@
-// const CRON_SECRET = process.env.CRON_SECRET
+// eslint-disable-next-line
+const axios = require('axios');
+
+const CRON_SECRET = process.env.CRON_SECRET;
 const BASE_URI = 'api/cron/';
 
-fetch(BASE_URI);
-export {};
+axios
+  .get(`http://localhost:3000/${BASE_URI}phases`, {
+    headers: {
+      CRON_SECRET: CRON_SECRET ?? 'secret',
+    },
+  })
+  // eslint-disable-next-line
+  .then((res) => console.log(res))
+  // eslint-disable-next-line
+  .catch((err) => console.log(err.toJSON()));
