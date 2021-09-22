30b4ec4810c0b1c2e08b3803bdad2cd0417b02f2
Zaim Imran <zaim.imran@gmail.com>
2021-09-22 15:42:54 +0200

    feat: upsert employess from blank to trak (#16)

--- src/pages/api/cron/fetchEmployees.ts
+++ src/pages/api/cron/fetchEmployees.ts
@@ -1,6 +1,5 @@
 import HttpStatusCode from 'http-status-typed';
 import { blankClient, trakClient } from 'lib/prisma';
-import { remove } from 'lodash';
 import { NextApiRequest, NextApiResponse } from 'next';
 export default async function (req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
@@ -20,10 +19,12 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
         },
       });
 
-      const MAGNE = await remove(blankEmployees, (employee) => employee.id === 2)[0];
-
-      await trakClient.employee.create({
-        data: {
+      const MAGNE = await blankEmployees.find((x) => x.id === 2);
+      const magne_connect_or_create = {
+        where: {
+          id: MAGNE.id,
+        },
+        create: {
           id: MAGNE.id,
           firstName: MAGNE.first_name,
           lastName: MAGNE.last_name,
@@ -42,32 +43,46 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
               },
             },
           },
-          employees: {
-            create: blankEmployees.map((employee) => {
-              return {
-                id: employee.id,
-                firstName: employee.first_name,
-                lastName: employee.last_name,
-                email: employee.email,
-                birthDate: employee.birth_date,
-                dateOfEmployment: employee.date_of_employment,
-                terminationDate: employee.termination_date,
-                imageUrl: employee.image_url,
-                profession: {
-                  connectOrCreate: {
-                    where: {
-                      title: employee.role,
-                    },
-                    create: {
-                      title: employee.role,
-                    },
-                  },
-                },
-              };
-            }),
-          },
         },
-      });
+      };
+
+      await trakClient.$transaction(
+        blankEmployees.map((employee) => {
+          const data = {
+            firstName: employee.first_name,
+            lastName: employee.last_name,
+            email: employee.email,
+            birthDate: employee.birth_date,
+            dateOfEmployment: employee.date_of_employment,
+            terminationDate: employee.termination_date,
+            imageUrl: employee.image_url,
+            hrManager: {
+              connectOrCreate: magne_connect_or_create,
+            },
+            profession: {
+              connectOrCreate: {
+                where: {
+                  title: employee.role,
+                },
+                create: {
+                  title: employee.role,
+                },
+              },
+            },
+          };
+
+          return trakClient.employee.upsert({
+            where: {
+              id: employee.id,
+            },
+            update: data,
+            create: {
+              id: employee.id,
+              ...data,
+            },
+          });
+        }),
+      );
 
       res.status(HttpStatusCode.OK).end();
     } catch (err) {
