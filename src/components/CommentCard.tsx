e7265a9afa8530cc54db47c8de4e3d172e2a29cb
Max Schau <max.torre.schau@gmail.com>
2022-03-03 16:01:40 +0100

    Fixed possbility for editing comment

--- src/components/CommentCard.tsx
+++ src/components/CommentCard.tsx
@@ -1,17 +1,23 @@
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import Grid, { GridProps } from '@mui/material/Grid';
 import IconButton from '@mui/material/IconButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import axios from 'axios';
 import Avatar from 'components/Avatar';
+import CommentEditor from 'components/form/CommentEditor';
 import { format } from 'date-fns';
 import { useSession } from 'next-auth/react';
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import ReactMarkdown from 'react-markdown';
-import { mutate } from 'swr';
+import { useSWRConfig } from 'swr';
 import { IComment } from 'utils/types';
 
 type CommentCardProps = {
@@ -20,22 +26,43 @@ type CommentCardProps = {
 
 const CommentCard = ({ comment, ...args }: CommentCardProps) => {
   const user = useSession();
-
+  const [displayEditComment, setDisplayEditComment] = useState(false);
   const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({ defaultValues: { comment: comment.text } });
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
   const handleClose = () => {
     setAnchorEl(null);
   };
+  const { mutate } = useSWRConfig();
+
+  const onSubmit = handleSubmit(async (formData) => {
+    await axios.put(`/api/employeeTasks/${comment.employeeTaskId}/comments/${comment.id}`, {
+      data: { ...comment, text: formData.comment },
+    });
+    mutate(`/api/employeeTasks/${comment.employeeTaskId}/comments`);
+    setDisplayEditComment(false);
+    handleClose();
+  });
 
   const deleteComment = async () => {
     await axios.delete(`/api/employeeTasks/${comment.employeeTaskId}/comments/${comment.id}`);
 
     mutate(`/api/employeeTasks/${comment.employeeTaskId}/comments`);
   };
-  return (
+
+  const editComment = () => {
+    setDisplayEditComment(true);
+  };
+  return displayEditComment ? (
+    <CommentEditor cancel={setDisplayEditComment} cancelText={'Avbryt'} confirmText={'Endre'} errors={errors} onSubmit={onSubmit} register={register} />
+  ) : (
     <Grid container marginBottom={2} spacing={2} {...args}>
       <Grid item sm={1} xs={2}>
         <Avatar firstName={comment.createdByEmployee.firstName} image={comment.createdByEmployee.imageUrl} lastName={comment.createdByEmployee.lastName} />
@@ -68,7 +95,18 @@ const CommentCard = ({ comment, ...args }: CommentCardProps) => {
               onClose={handleClose}
               open={open}
             >
-              <MenuItem onClick={deleteComment}>Slett kommentar</MenuItem>
+              <MenuItem onClick={editComment}>
+                <ListItemIcon>
+                  <EditIcon color='primary' />
+                </ListItemIcon>
+                <ListItemText primary='Rediger' />
+              </MenuItem>
+              <MenuItem onClick={deleteComment}>
+                <ListItemIcon>
+                  <DeleteIcon color='primary' />
+                </ListItemIcon>
+                <ListItemText primary='Slett' />
+              </MenuItem>
             </Menu>
           </>
         )}
